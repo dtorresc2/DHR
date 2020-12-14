5979adac9576c835e39b1f9a36364700665135ea
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-12-13 20:42:15 -0600

    Ajustes navegacion 3

---
+++ app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Catalogos.java
@@ -0,0 +1,27 @@
+package com.example.dentalhistoryrecorder.Rutas.Catalogos;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dentalhistoryrecorder.R;
+
+
+public class Catalogos extends Fragment {
+
+
+    public Catalogos() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_catalogos, container, false);
+        return view;
+    }
+}
