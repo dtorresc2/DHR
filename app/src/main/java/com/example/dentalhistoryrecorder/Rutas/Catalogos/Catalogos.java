952b9eccc2c599df5be5eb354c8cb17d94e01c22
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-12-13 21:43:15 -0600

    Catalogos ajustes 1

--- app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Catalogos.java
+++ app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Catalogos.java
@@ -1,16 +1,20 @@
 package com.example.dentalhistoryrecorder.Rutas.Catalogos;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.dentalhistoryrecorder.R;
 
 
 public class Catalogos extends Fragment {
-
+    TextView titulo, texto_pacientes, texto_servicios, texto_piezas, texto_cuentas;
+    LinearLayout opcion_pacientes, opcion_servicios, opcion_piezas, opcion_cuentas;
 
     public Catalogos() {
         // Required empty public constructor
@@ -22,6 +26,23 @@ public class Catalogos extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_catalogos, container, false);
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/bahnschrift.ttf");
+
+        titulo = view.findViewById(R.id.cat_titulo);
+        titulo.setTypeface(typeface);
+
+        texto_pacientes = view.findViewById(R.id.cat_texto_pacientes);
+        texto_pacientes.setTypeface(typeface);
+
+        texto_servicios = view.findViewById(R.id.cat_texto_servicios);
+        texto_servicios.setTypeface(typeface);
+
+        texto_piezas = view.findViewById(R.id.cat_texto_piezas);
+        texto_piezas.setTypeface(typeface);
+
+        texto_cuentas = view.findViewById(R.id.cat_texto_cuentas);
+        texto_cuentas.setTypeface(typeface);
+
         return view;
     }
 }
