fc3eafedda9a18374962ba79e6c072baabf09b20
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-12-15 20:36:15 -0600

    Catalogo Servicios 7

--- app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/ServiciosAdapter.java
+++ app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/ServiciosAdapter.java
@@ -7,8 +7,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.dentalhistoryrecorder.R;
@@ -68,11 +66,9 @@ public class ServiciosAdapter extends RecyclerView.Adapter<ServiciosAdapter.Serv
 
         if (i < mlistaServicios.size()) {
             serviciosViewHolder.separador.setVisibility(View.VISIBLE);
-        }
-        else {
+        } else {
             serviciosViewHolder.separador.setVisibility(View.INVISIBLE);
         }
-
     }
 
     @Override
