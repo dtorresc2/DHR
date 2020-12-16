1883d215493c3f6aab896ac5b6af1365149a29ce
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-12-15 20:28:02 -0600

    Catalogo Servicios 6

--- app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/ServiciosAdapter.java
+++ app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/ServiciosAdapter.java
@@ -1,11 +1,14 @@
 package com.example.dentalhistoryrecorder.Rutas.Catalogos.Servicios;
 
 import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.dentalhistoryrecorder.R;
@@ -14,14 +17,18 @@ import java.util.ArrayList;
 
 public class ServiciosAdapter extends RecyclerView.Adapter<ServiciosAdapter.ServiciosViewHolder> {
     private ArrayList<ItemServicio> mlistaServicios;
-    public static class ServiciosViewHolder extends  RecyclerView.ViewHolder {
+    private ViewGroup mViewGroup;
+
+    public static class ServiciosViewHolder extends RecyclerView.ViewHolder {
         public TextView descripcion, monto, estado;
+        public View separador;
 
         public ServiciosViewHolder(@NonNull View itemView) {
             super(itemView);
             descripcion = itemView.findViewById(R.id.tituloServicio);
             monto = itemView.findViewById(R.id.montoServicio);
             estado = itemView.findViewById(R.id.estadoServicio);
+            separador = itemView.findViewById(R.id.separadorServicio);
         }
     }
 
@@ -32,8 +39,14 @@ public class ServiciosAdapter extends RecyclerView.Adapter<ServiciosAdapter.Serv
     @NonNull
     @Override
     public ServiciosViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        mViewGroup = viewGroup;
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_servicio, viewGroup, false);
+        Typeface typeface = Typeface.createFromAsset(view.getContext().getAssets(), "fonts/bahnschrift.ttf");
+
         ServiciosViewHolder serviciosViewHolder = new ServiciosViewHolder(view);
+        serviciosViewHolder.descripcion.setTypeface(typeface);
+        serviciosViewHolder.monto.setTypeface(typeface);
+        serviciosViewHolder.estado.setTypeface(typeface);
         return serviciosViewHolder;
     }
 
@@ -47,12 +60,19 @@ public class ServiciosAdapter extends RecyclerView.Adapter<ServiciosAdapter.Serv
 
         if (itemServicio.getEstadoServicio()) {
             serviciosViewHolder.estado.setText("Habilitado");
-            serviciosViewHolder.estado.setBackgroundColor(R.color.VerdeOscuro);
+            serviciosViewHolder.estado.setBackgroundColor(mViewGroup.getContext().getResources().getColor(R.color.VerdeOscuro));
+        } else {
+            serviciosViewHolder.estado.setText("Deshabilitado");
+            serviciosViewHolder.estado.setBackgroundColor(mViewGroup.getContext().getResources().getColor(R.color.RojoOscuro));
+        }
+
+        if (i < mlistaServicios.size()) {
+            serviciosViewHolder.separador.setVisibility(View.VISIBLE);
         }
         else {
-            serviciosViewHolder.estado.setText("Deshabilitado");
-            serviciosViewHolder.estado.setBackgroundColor(R.color.RojoOscuro);
+            serviciosViewHolder.separador.setVisibility(View.INVISIBLE);
         }
+
     }
 
     @Override
