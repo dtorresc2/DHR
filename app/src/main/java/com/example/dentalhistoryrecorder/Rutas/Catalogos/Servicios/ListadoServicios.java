ef01d77e1c00395f593cfb1044fc0bb21d0c0ffe
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-12-15 21:02:58 -0600

    Catalogo Servicios 9

--- app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/ListadoServicios.java
+++ app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/ListadoServicios.java
@@ -1,8 +1,10 @@
 package com.example.dentalhistoryrecorder.Rutas.Catalogos.Servicios;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,10 +13,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.dentalhistoryrecorder.OpcionConsulta.Normal.ItemsFichas;
 import com.example.dentalhistoryrecorder.R;
 import com.example.dentalhistoryrecorder.ServiciosAPI.QuerysCuentas;
 import com.example.dentalhistoryrecorder.ServiciosAPI.QuerysServicios;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -35,20 +39,33 @@ public class ListadoServicios extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_listado_servicios, container, false);
 
-        ArrayList<ItemServicio> listaServicios = new ArrayList<>();
-        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, true));
-        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, true));
-        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, true));
-        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, false));
-        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, false));
+        final ArrayList<ItemServicio> listaServicios = new ArrayList<>();
+//        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, true));
+//        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, true));
+//        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, true));
+//        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, false));
+//        listaServicios.add(new ItemServicio(1, "Hola Es una prueba", 25.00, false));
 
         mRecyclerView = view.findViewById(R.id.listado_servicios);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(getContext());
-        mAdapter = new ServiciosAdapter(listaServicios);
+//        mAdapter = new ServiciosAdapter(listaServicios);
+//
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//        mRecyclerView.setAdapter(mAdapter);
 
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+        final ProgressDialog progressDialog = new ProgressDialog(getContext(), R.style.progressDialog);
+        progressDialog.setMessage("Cargando...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+//        Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            public void run() {
+//                progressDialog.dismiss();
+//            }
+//        }, 1000);
 
         final SharedPreferences preferenciasUsuario = getActivity().getSharedPreferences("sesion", Context.MODE_PRIVATE);
 
@@ -56,7 +73,23 @@ public class ListadoServicios extends Fragment {
         querysServicios.obtenerListadoServicios(preferenciasUsuario.getInt("ID_USUARIO", 0), new QuerysServicios.VolleyOnEventListener() {
             @Override
             public void onSuccess(Object object) {
-                Toast.makeText(getContext(), object.toString(), Toast.LENGTH_LONG).show();
+                try {
+                    JSONArray jsonArray = new JSONArray(object.toString());
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        listaServicios.add(new ItemServicio(
+                                jsonArray.getJSONObject(i).getInt("ID_SERVICIO"),
+                                jsonArray.getJSONObject(i).getString("DESCRIPCION"),
+                                jsonArray.getJSONObject(i).getDouble("MONTO"),
+                                (jsonArray.getJSONObject(i).getInt("ESTADO") > 0) ? true : false
+                        ));
+                    }
+                    mAdapter = new ServiciosAdapter(listaServicios);
+                    mRecyclerView.setLayoutManager(mLayoutManager);
+                    mRecyclerView.setAdapter(mAdapter);
+                    progressDialog.dismiss();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
