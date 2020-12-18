e44ddfc7620e48a13bb51351e19e479ae917e360
dtorresc2 <dtorresc2@miumg.edu.gt>
2020-12-17 20:01:26 -0600

    Servicios - form 1

--- app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/Servicios.java
+++ app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/Servicios.java
@@ -1,21 +1,39 @@
 package com.example.dentalhistoryrecorder.Rutas.Catalogos.Servicios;
 
+import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.example.dentalhistoryrecorder.R;
 import com.example.dentalhistoryrecorder.Rutas.Catalogos.Catalogos;
+import com.itextpdf.text.xml.simpleparser.EntitiesToUnicode;
 
 public class Servicios extends Fragment {
     private Toolbar toolbar;
+    private TextInputEditText descripcionServicio, montoServicio;
+    private RadioButton trueServicio, falseServicio;
+    private FloatingActionButton guardadorServicio;
+    private TextView tituloServicio;
+
+    private boolean modoEdicion;
+//    private Typeface typeface;
 
     public Servicios() {
-        // Required empty public constructor
+        modoEdicion = false;
+    }
+
+    public void editarServicio(int id) {
+        modoEdicion = true;
     }
 
     @Override
@@ -23,6 +41,8 @@ public class Servicios extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_servicios, container, false);
+        Typeface typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/bahnschrift.ttf");
+
         toolbar = view.findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_cerrar);
         toolbar.setTitle("Servicio Nuevo");
@@ -34,6 +54,29 @@ public class Servicios extends Fragment {
                 FragmentTransaction transaction = getFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
                 transaction.replace(R.id.contenedor, listadoServicios);
                 transaction.commit();
+            }
+        });
+
+        descripcionServicio = view.findViewById(R.id.descripcionServicio);
+        descripcionServicio.setTypeface(typeface);
+
+        montoServicio = view.findViewById(R.id.montoServicio);
+        montoServicio.setTypeface(typeface);
+
+        tituloServicio = view.findViewById(R.id.tituloEstadoServicio);
+        tituloServicio.setTypeface(typeface);
+
+        trueServicio = view.findViewById(R.id.trueServicio);
+        trueServicio.setTypeface(typeface);
+
+        falseServicio = view.findViewById(R.id.falseServicio);
+        falseServicio.setTypeface(typeface);
+
+        guardadorServicio = view.findViewById(R.id.grabarServicio);
+        guardadorServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
 
