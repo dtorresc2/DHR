5606fe0f8f0ca96952477cc71d3e9a074e364a94
Diego Torres <dtorresc2@miumg.ed.gt>
2020-12-23 19:41:20 -0600

    Servicios - Editar Servicio 2

--- app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/Servicios.java
+++ app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Servicios/Servicios.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -110,7 +113,9 @@ public class Servicios extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                montoRequerido();
+                if (montoRequerido()) {
+                    validarMonto();
+                }
             }
 
             @Override
@@ -128,6 +133,10 @@ public class Servicios extends Fragment {
         falseServicio = view.findViewById(R.id.falseServicio);
         falseServicio.setTypeface(typeface);
 
+        if (modoEdicion) {
+            obtenerServicio();
+        }
+
         guardadorServicio = view.findViewById(R.id.grabarServicio);
         guardadorServicio.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,7 +144,10 @@ public class Servicios extends Fragment {
                 if (!descripcionRequerida() || !validarDescripcion() || !montoRequerido())
                     return;
 
-                registrarServicio();
+                if (!modoEdicion)
+                    registrarServicio();
+                else
+                    actualizarServicio();
             }
         });
 
@@ -189,6 +201,57 @@ public class Servicios extends Fragment {
         });
     }
 
+    private void actualizarServicio() {
+        final ProgressDialog progressDialog = new ProgressDialog(getContext(), R.style.progressDialog);
+        progressDialog.setMessage("Cargando...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+//        final SharedPreferences preferenciasUsuario = getActivity().getSharedPreferences("sesion", Context.MODE_PRIVATE);
+
+        QuerysServicios querysServicios = new QuerysServicios(getContext());
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("DESCRIPCION", descripcionServicio.getText().toString().trim());
+            jsonBody.put("MONTO", montoServicio.getText().toString().trim());
+            jsonBody.put("ESTADO", (trueServicio.isChecked() == true) ? 1 : 0);
+//            jsonBody.put("ID_USUARIO", preferenciasUsuario.getInt("ID_USUARIO", 0));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        querysServicios.actualizarServicio(ID_SERVICIO, jsonBody, new QuerysServicios.VolleyOnEventListener() {
+            @Override
+            public void onSuccess(Object object) {
+                Alerter.create(getActivity())
+                        .setTitle("Servicio Actualizado")
+                        .setIcon(R.drawable.logonuevo)
+                        .setTextTypeface(typeface)
+                        .enableSwipeToDismiss()
+                        .setBackgroundColorRes(R.color.FondoSecundario)
+                        .show();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+                        ListadoServicios listadoServicios = new ListadoServicios();
+                        FragmentTransaction transaction = getFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+                        transaction.replace(R.id.contenedor, listadoServicios);
+                        transaction.commit();
+                    }
+                }, 1000);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(getContext(), e.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private void obtenerServicio() {
         final ProgressDialog progressDialog = new ProgressDialog(getContext(), R.style.progressDialog);
         progressDialog.setMessage("Cargando...");
@@ -200,23 +263,26 @@ public class Servicios extends Fragment {
         querysServicios.obtenerServicioEspecifico(ID_SERVICIO, new QuerysServicios.VolleyOnEventListener() {
             @Override
             public void onSuccess(Object object) {
+                progressDialog.dismiss();
                 try {
-                    JSONArray jsonArray = new JSONArray(object.toString());
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        descripcionServicio.setText(jsonArray.getJSONObject(i).getString("DESCRIPCION"));
-                        montoServicio.setText(String.format("%.2f", jsonArray.getJSONObject(i).getDouble("MONTO")));
-                        boolean habilitado = ((jsonArray.getJSONObject(i).getInt("ESTADO")) > 0 ? true : false);
-                        trueServicio.setSelected(habilitado);
-                        falseServicio.setSelected(!habilitado);
-                    }
+                    JSONObject jsonObject = new JSONObject(object.toString());
+                    descripcionServicio.setText(jsonObject.getString("DESCRIPCION"));
+                    montoServicio.setText(String.format("%.2f", jsonObject.getDouble("MONTO")));
+                    boolean habilitado = ((jsonObject.getInt("ESTADO")) > 0 ? true : false);
+                    trueServicio.setChecked(habilitado);
+                    falseServicio.setChecked(!habilitado);
                 } catch (JSONException e) {
+                    progressDialog.dismiss();
+                    Log.i("SERVICIO", e.toString());
                     e.printStackTrace();
                 }
             }
 
             @Override
             public void onFailure(Exception e) {
-
+                progressDialog.dismiss();
+                Log.i("SERVICIO", e.toString());
+                e.printStackTrace();
             }
         });
     }
@@ -255,4 +321,18 @@ public class Servicios extends Fragment {
             return false;
         }
     }
+
+    private boolean validarMonto() {
+        String textoDescripcion = montoServicio.getText().toString().trim();
+        Pattern patron = Pattern.compile("^[0-9]+(\\.[0-9]{2})$");
+        if (patron.matcher(textoDescripcion).matches()) {
+            montoServicio.setError(null);
+            return true;
+        } else {
+            montoServicio.setError("Monto invalido, debe usar ####.##");
+            return false;
+        }
+    }
+
+//    '^[0-9]+(\.[0-9]{2})$'
 }
--- app/src/main/java/com/example/dentalhistoryrecorder/ServiciosAPI/QuerysServicios.java
+++ app/src/main/java/com/example/dentalhistoryrecorder/ServiciosAPI/QuerysServicios.java
@@ -112,4 +112,39 @@ public class QuerysServicios {
         requestQueue.add(stringRequest);
     }
 
+    public void actualizarServicio(final int id, final JSONObject jsonBody, QuerysServicios.VolleyOnEventListener callback) {
+        mCallBack = callback;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.PUT, mContext.getResources().getString(R.string.API) + "servicios/" + id, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) { ;
+                mCallBack.onSuccess(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mCallBack.onFailure(error);
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() {
+                try {
+                    final String mRequestBody = jsonBody.toString();
+                    return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+        requestQueue.add(stringRequest);
+    }
+
 }
