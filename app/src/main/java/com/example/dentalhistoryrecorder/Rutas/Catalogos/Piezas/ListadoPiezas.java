6ff829d88049006ea6f4273f617ce9296227c7a1
Diego Torres <dtorresc2@miumg.ed.gt>
2020-12-27 13:12:09 -0600

    Catalogo Piezas - Listado 2

--- app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Piezas/ListadoPiezas.java
+++ app/src/main/java/com/example/dentalhistoryrecorder/Rutas/Catalogos/Piezas/ListadoPiezas.java
@@ -2,13 +2,18 @@ package com.example.dentalhistoryrecorder.Rutas.Catalogos.Piezas;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dentalhistoryrecorder.R;
+import com.example.dentalhistoryrecorder.Rutas.Catalogos.Catalogos;
 
 public class ListadoPiezas extends Fragment {
+    private Toolbar toolbar;
 
     public ListadoPiezas() {
         // Required empty public constructor
@@ -19,6 +24,29 @@ public class ListadoPiezas extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_listado_piezas, container, false);
+
+        toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_cerrar);
+        toolbar.setTitle("Piezas");
+        toolbar.inflateMenu(R.menu.opciones_toolbar_catalogos);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Catalogos catalogos = new Catalogos();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+                transaction.replace(R.id.contenedor, catalogos);
+                transaction.commit();
+            }
+        });
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
         return view;
     }
 }
