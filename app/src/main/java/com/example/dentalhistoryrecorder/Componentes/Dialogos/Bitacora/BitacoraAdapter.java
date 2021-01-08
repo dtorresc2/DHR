4ced2227d2b3815fe6395f32c3697465ba8168f5
Diego Torres <dtorresc2@miumg.ed.gt>
2021-01-07 22:20:09 -0600

    Bitacora 4

--- app/src/main/java/com/example/dentalhistoryrecorder/Componentes/Dialogos/Bitacora/BitacoraAdapter.java
+++ app/src/main/java/com/example/dentalhistoryrecorder/Componentes/Dialogos/Bitacora/BitacoraAdapter.java
@@ -1,4 +1,15 @@
 package com.example.dentalhistoryrecorder.Componentes.Dialogos.Bitacora;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 public class BitacoraAdapter {
+
+    public static class BitacoraViewHolder extends RecyclerView.ViewHolder {
+
+        public BitacoraViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
