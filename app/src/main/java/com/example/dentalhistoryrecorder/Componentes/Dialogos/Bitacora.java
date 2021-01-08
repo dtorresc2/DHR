e9b8ee523108755e9ad8211baa30ba63a60cf545
Diego Torres <dtorresc2@miumg.ed.gt>
2021-01-07 21:55:35 -0600

    Bitacora 1

---
+++ app/src/main/java/com/example/dentalhistoryrecorder/Componentes/Dialogos/Bitacora.java
@@ -0,0 +1,75 @@
+package com.example.dentalhistoryrecorder.Componentes.Dialogos;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dentalhistoryrecorder.R;
+
+public class Bitacora extends DialogFragment {
+    private Toolbar toolbar;
+
+    public Bitacora() {
+
+    }
+
+    public static Bitacora display(FragmentManager fragmentManager) {
+        Bitacora bitacora = new Bitacora();
+        bitacora.show(fragmentManager, "Dialogo Bitacora");
+        return bitacora;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.AppTheme_FullScreenDialog);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+            dialog.getWindow().setWindowAnimations(R.style.AppTheme_Slide);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.dialogo_bitacora, container, false);
+
+        toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setTitle("Bitacora");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.Blanco));
+
+        toolbar.inflateMenu(R.menu.opciones_toolbarcitas);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_aceptar:
+                        dismiss();
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        return view;
+    }
+}
