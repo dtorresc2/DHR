009feac1bd40262ca83f1a0e29cf5707d749a838
Diego Torres <dtorresc2@miumg.ed.gt>
2021-04-02 19:20:49 -0600

    Ficha - Historial Fotografico 2

--- app/src/main/java/com/sistemasdt/dhr/Rutas/Fichas/HistorialFoto/FotoAdapter.java
+++ app/src/main/java/com/sistemasdt/dhr/Rutas/Fichas/HistorialFoto/FotoAdapter.java
@@ -1,18 +1,40 @@
 package com.sistemasdt.dhr.Rutas.Fichas.HistorialFoto;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
-public class FotoAdapter extends RecyclerView.Adapter<> {
+import com.sistemasdt.dhr.R;
+
+import java.util.List;
+
+public class FotoAdapter extends RecyclerView.Adapter<FotoAdapter.ViewHolderConsulta> {
+    private List<String> imageList;
+    private Context c;
+
+    public static class ViewHolderConsulta extends RecyclerView.ViewHolder{
+        SquareImageView siv;
+        public ViewHolderConsulta(@NonNull View itemView) {
+            super(itemView);
+            siv = itemView.findViewById(R.id.siv);
+        }
+    }
+
+    public FotoAdapter(Context c, List imageList) {
+        this.c = c;
+        this.imageList = imageList;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public ViewHolderConsulta onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolderConsulta viewHolderConsulta, int i) {
 
     }
 
